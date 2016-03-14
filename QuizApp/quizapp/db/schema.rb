09758bf3ea33c91d379b529cb7bab860914dbefc
montselozanod <montse_967@hotmail.com>
2016-03-14 00:43:22 -0600

    add foundation and better-errors

--- QuizApp/quizapp/db/schema.rb
+++ QuizApp/quizapp/db/schema.rb
@@ -11,10 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314060751) do
+ActiveRecord::Schema.define(version: 20160314063604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: true do |t|
+    t.text     "textAnswer"
+    t.boolean  "active"
+    t.string   "imgURL"
+    t.integer  "pregunta_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "answers", ["pregunta_id"], name: "index_answers_on_pregunta_id", using: :btree
+
+  create_table "exposicions", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "imgURL"
+    t.string   "type"
+    t.boolean  "active"
+    t.integer  "museo_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "exposicions", ["museo_id"], name: "index_exposicions_on_museo_id", using: :btree
 
   create_table "museos", force: true do |t|
     t.string   "name"
@@ -23,5 +47,31 @@ ActiveRecord::Schema.define(version: 20160314060751) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "pregunta", force: true do |t|
+    t.text     "description"
+    t.text     "extraInfo"
+    t.string   "imgURL"
+    t.boolean  "active"
+    t.integer  "sala_id"
+    t.integer  "answer_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "pregunta", ["answer_id"], name: "index_pregunta_on_answer_id", using: :btree
+  add_index "pregunta", ["sala_id"], name: "index_pregunta_on_sala_id", using: :btree
+
+  create_table "salas", force: true do |t|
+    t.integer  "number"
+    t.string   "name"
+    t.string   "mapImgURL"
+    t.text     "description"
+    t.integer  "exposicion_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "salas", ["exposicion_id"], name: "index_salas_on_exposicion_id", using: :btree
 
 end
