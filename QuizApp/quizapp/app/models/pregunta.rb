fbc4d983c51b842c23872d5796f4730ebaa61b5e
montselozanod <montse_967@hotmail.com>
2016-03-15 11:08:40 -0600

    check correct answer

--- QuizApp/quizapp/app/models/pregunta.rb
+++ QuizApp/quizapp/app/models/pregunta.rb
@@ -1,4 +1,16 @@
 class Pregunta < ActiveRecord::Base
   belongs_to :sala
   has_many :answers
+
+  accepts_nested_attributes_for :answers, allow_destroy: true
+
+  def correct_answer
+    answers.where(correct: true)
+  end
+
+  def check(answer)
+    check_answers = correct_answer.map(&:id)
+    check_answers.include? answer.to_i
+  end
+
 end
