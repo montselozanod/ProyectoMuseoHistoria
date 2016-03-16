1385e17b0aa1965b30ced8dde82fc7c7921f6327
montselozanod <montse_967@hotmail.com>
2016-03-15 22:52:14 -0600

    salas

--- QuizApp/quizapp/config/routes.rb
+++ QuizApp/quizapp/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :museos
   resources :exposicions
+  resources :salas
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
