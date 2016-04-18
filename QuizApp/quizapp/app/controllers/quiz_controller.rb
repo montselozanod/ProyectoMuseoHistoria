c08d9271496d985c9fc34a24b70b556288e17f9e
montselozanod <montse_967@hotmail.com>
2016-04-18 00:55:58 -0500

    add answer method

--- QuizApp/quizapp/app/controllers/quiz_controller.rb
+++ QuizApp/quizapp/app/controllers/quiz_controller.rb
@@ -1,6 +1,8 @@
 class QuizController < ApplicationController
   before_filter :authenticate_user!
   def index
+    current_user.currentPoints = 0
+    current_user.save
     @museos = Museo.all
   end
 end
