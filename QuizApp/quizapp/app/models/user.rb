1c666bff9ee7b408292f54afba5b80bc994c214f
montselozanod <montse_967@hotmail.com>
2016-04-25 14:28:26 -0500

    poner en bold

--- QuizApp/quizapp/app/models/user.rb
+++ QuizApp/quizapp/app/models/user.rb
@@ -5,10 +5,4 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :trypoints, :dependent => :destroy
 
-  enum role: [:user, :admin]
-  after_initialize :set_default_role, :if => :new_record?
-
-  def set_default_role
-    self.role ||= :user
-  end
 end
