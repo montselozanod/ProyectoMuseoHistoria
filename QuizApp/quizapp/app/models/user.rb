7d441a019cb74eefed50f884a2a5af61f7d36ab5
montselozanod <montse_967@hotmail.com>
2016-04-25 14:09:12 -0500

    define role in user

--- QuizApp/quizapp/app/models/user.rb
+++ QuizApp/quizapp/app/models/user.rb
@@ -4,4 +4,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :trypoints, :dependent => :destroy
+
+  enum role: [:user, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
